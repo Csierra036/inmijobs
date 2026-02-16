0b6e2ac54cd1d21ef57d23b0c3d270e0f3947a7a
Cristian Sierra <cristiansierra.rap@gmail.com>
2026-02-15 22:14:54 -0400

    remove: innecesary comments

--- backend-core/internal/repository/comment_repository.go
+++ backend-core/internal/repository/comment_repository.go
@@ -17,7 +17,6 @@ func NewCommentRepository(db gorm.DB) *CommentRepository {
 	return &CommentRepository{db: db}
 }
 
-// Create comment
 func (r CommentRepository) Create(ctx context.Context, comment model.Comment) (dto.CommentResponse, error) {
 	if err := gorm.G[model.Comment](&r.db).Create(ctx, &comment); err != nil {
 		return dto.CommentResponse{}, fmt.Errorf("repository: create comment: %w", err)
@@ -28,31 +27,27 @@ func (r CommentRepository) Create(ctx context.Context, comment model.Comment) (d
 		Message:   comment.Content,
 		PostID:    comment.PostID,
 		UserID:    comment.UserID,
-		CreatedAt: comment.CreatedAt, // ✅ llama al método Time() y luego Unix()
+		CreatedAt: comment.CreatedAt,
 		UpdatedAt: comment.UpdatedAt,
 	}
 
 	return resp, nil
 }
 
-// Update comment
 func (r CommentRepository) Update(ctx context.Context, CommentID string, comment dto.UpdateCommentRequest) (model.Comment, error) {
-	// verificar existencia
+
 	var existing model.Comment
 	if _, err := gorm.G[model.Comment](&r.db).Where("id = ?", CommentID).First(ctx); err != nil {
 		return model.Comment{}, fmt.Errorf("repository: find comment before update: %w", err)
 	}
 
-	// actualizar campos según DTO
 	if comment.Content != "" {
 		existing.Content = comment.Content
 	}
 	if comment.Content != "" {
 		existing.Content = comment.Content
 	}
-	// agrega otros campos según tu DTO
 
-	// guardar cambios
 	_, err := gorm.G[model.Comment](&r.db).Where("id = ?", CommentID).Updates(ctx, existing)
 	if err != nil {
 		return model.Comment{}, fmt.Errorf("repository: update comment: %w", err)
@@ -61,7 +56,6 @@ func (r CommentRepository) Update(ctx context.Context, CommentID string, comment
 	return existing, nil
 }
 
-// Delete comment
 func (r CommentRepository) Delete(ctx context.Context, id string) error {
 	_, err := gorm.G[model.Comment](&r.db).
 		Where("id = ?", id).
@@ -74,7 +68,6 @@ func (r CommentRepository) Delete(ctx context.Context, id string) error {
 	return nil
 }
 
-// Get by id comment
 func (r CommentRepository) GetByID(ctx context.Context, id string) (model.Comment, error) {
 	comment, err := gorm.G[model.Comment](&r.db).Where("id = ?", id).First(ctx)
 	if err != nil {
@@ -83,7 +76,6 @@ func (r CommentRepository) GetByID(ctx context.Context, id string) (model.Commen
 	return comment, nil
 }
 
-// Finder comment
 func (r CommentRepository) List(ctx context.Context, limit int) ([]model.Comment, error) {
 
 	comments, err := gorm.G[model.Comment](&r.db).
